d66464e86f6539ea09297e1f53771a056ca60564
Paptop <iljajurchenko@email.com>
2019-12-03 20:41:53 +0200

    Initial Commit

---
+++ Zen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Zen
+//
+//  Created by Ilja Jurchenko on 03/12/2019.
+//  Copyright © 2019 company. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
